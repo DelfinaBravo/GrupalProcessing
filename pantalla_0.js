b165598305f9ebd7bbd1830ec56e86cac5e55577
Santiago Duran <Santiagoduran2006@gmail.com>
2024-06-02 23:35:41 -0300

    pantalla_0

--- pantalla_0.js
+++ pantalla_0.js
@@ -1,26 +1,31 @@
 let imagenFondo; // Variable para almacenar la imagen de fondo
 let boton;
+let logo;
+
 function setup() {
   imagenFondo = loadImage('fondo.jpg');
+  logo = loadImage('logo.png');
   createCanvas(1366, 768);
   boton = createButton('ENTRAR AL JUEGO');
 }
 
 function draw() {
   background(imagenFondo);
-  BotonEntrar(); 
+  BotonEntrar();
+  image(logo,450,-40);
+}
+
+function BotonEntrar(){
+  //toda la funcion para configurar al método del boton.
+  boton.position(555,500);
+  boton.size(300,100);
+  boton.style('border', '3px solid red');
+  boton.style('font-size', '32px');
+  boton.mousePressed(Entrar);
 }
 
 function Entrar(){
   boton.hide();//el método hide sirve para que el elemento deje ser visible 
   //y el método show() sirve para mostrar nuevamente.
-}
-
-function BotonEntrar(){
-  //toda la funcion para configurar al método del boton.
-  boton.position(550,500);
-  boton.size(300,100);
-  boton.style('border', '3px solid red');
-  boton.style('font-size', '32px');
-  boton.mousePressed(Entrar);
+  logo = false;  // deja de mostrar el logo.
 }
