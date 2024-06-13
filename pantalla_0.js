2cae09210b40d900fdb737b7f3e699950943dbbd
Santiago Duran <Santiagoduran2006@gmail.com>
2024-06-13 14:43:49 -0300

    en blanco

--- pantalla_0.js
+++ pantalla_0.js
@@ -1,31 +1,68 @@
 let imagenFondo; // Variable para almacenar la imagen de fondo
 let boton;
 let logo;
+let mostrarLogo = true;
+let valorDesvanecer = 0;
+let desvaneciendoEn = false;
+let desvaneciendoFuera = false;
+let tiempoInicioDesvanecer;
+
 
 function setup() {
+  createCanvas(1366, 768);
   imagenFondo = loadImage('fondo.jpg');
   logo = loadImage('logo.png');
-  createCanvas(1366, 768);
   boton = createButton('ENTRAR AL JUEGO');
+  configurarBoton();
 }
 
 function draw() {
   background(imagenFondo);
-  BotonEntrar();
-  image(logo,450,-40);
+  if (mostrarLogo) {
+    image(logo, 450, -40);
+  }
+
+  manejarEfectoDesvanecer();
 }
 
-function BotonEntrar(){
-  //toda la funcion para configurar al método del boton.
-  boton.position(555,500);
-  boton.size(300,100);
+function configurarBoton(){
+  boton.position(555, 500);
+  boton.size(300, 100);
   boton.style('border', '3px solid red');
   boton.style('font-size', '32px');
-  boton.mousePressed(Entrar);
+  boton.mousePressed(entrar);
 }
 
-function Entrar(){
-  boton.hide();//el método hide sirve para que el elemento deje ser visible 
-  //y el método show() sirve para mostrar nuevamente.
-  logo = false;  // deja de mostrar el logo.
+function entrar(){
+  boton.hide(); // Oculta el botón
+  mostrarLogo = false; // Oculta el logo
+  iniciarEfectoDesvanecer();
+}
+
+function manejarEfectoDesvanecer() {
+  if (desvaneciendoEn) {
+    valorDesvanecer = map(millis() - tiempoInicioDesvanecer, 0, 1500, 0, 255); // Aumenta opacidad en 1.5 segundos
+    if (valorDesvanecer >= 255) {
+      valorDesvanecer = 255;
+      desvaneciendoEn = false;
+      desvaneciendoFuera = true;
+      tiempoInicioDesvanecer = millis();
+    }
+  } else if (desvaneciendoFuera) {
+    valorDesvanecer = map(millis() - tiempoInicioDesvanecer, 0, 1500, 255, 0); // Disminuye opacidad en 1.5 segundos
+    if (valorDesvanecer <= 0) {
+      valorDesvanecer = 0;
+      desvaneciendoFuera = false;
+    }
+  }
+
+  if (desvaneciendoEn || desvaneciendoFuera) {
+    fill(255, valorDesvanecer);
+    rect(0, 0, width, height); // Dibuja un rectángulo blanco con opacidad variable
+  }
+}
+
+function iniciarEfectoDesvanecer() {
+  desvaneciendoEn = true;
+  tiempoInicioDesvanecer = millis();
 }
