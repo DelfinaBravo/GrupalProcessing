dc6403bc7a4466d2a2b968ffa5f29bf04612ef78
Santiago Duran <Santiagoduran2006@gmail.com>
2024-06-18 19:19:23 -0300

    audios

--- pantalla_0/pantalla_0.js
+++ pantalla_0/pantalla_0.js
@@ -1,34 +1,38 @@
 let imagenFondo; 
 let logo;
 let boton;
+let audio;
 
 function setup() {
   createCanvas(1366, 768);
   imagenFondo = loadImage('../imagenes/imgp0/fondo.png');
   logo = loadImage('../imagenes/imgp0/logo.png');
-
+  audio = createAudio('../media/botonaudio.mp3');
+  
+  // Crear y configurar el botón una vez
+  boton = createButton('INICIAR JUEGO');
+  configurarBoton();
 }
 
 function draw() {
   background(imagenFondo);
-  
   image(logo, 450, -40);
-  boton = createButton('INICIAR JUEGO');  
-  configurarBoton();
 }
 
-function configurarBoton(){
+function configurarBoton() {
   boton.position(555, 500);
   boton.size(300, 100);
   boton.style('border', '3px solid black');
   boton.style('font-size', '32px');
-  boton.style('background','yellow');
-  // Asociar el botón a la otra página HTML
-  // Cuando el boton es presionado rederecciona a la otra pantalla0.
-  boton.mousePressed(() => {
-   
-    window.location.href = '../pantalla_1/index.html';
-  });
+  boton.style('background', 'yellow');
+  boton.mousePressed(BotonAudio);
+}
 
+function BotonAudio() {
+  audio.play();
+  // Retrasar la redirección para asegurar que el audio comienza a reproducirse
+  // setTimeout es una función de JavaScript que ejecuta una función después de un retraso
+  // especificado en milisegundos.
+  setTimeout(()=>{window.location.href = '../pantalla_1/index.html';},1000); // 1 segundo de retraso
 }
 
