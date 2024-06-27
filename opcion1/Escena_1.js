3ab5450d0c975451331e8834d28700a0616d029b
Santiago Duran <Santiagoduran2006@gmail.com>
2024-06-26 23:46:36 -0300

    h

--- opcion1/Escena_1.js
+++ opcion1/Escena_1.js
@@ -8,39 +8,39 @@ let pulgar;
 let moisesDesaparecido = false; // Variable para rastrear si Moisés ha desaparecido
 let audioD;
 function preload() {
-//  audioD= createButton('../audio/multitudpersonas.mp3');
-
+  audioD= createAudio('../audio/multitudpersonas.mp3');
+  moises = loadImage('../Media_ChavezFernanda/moisesvara.png');
+  multitud = loadImage('../mediaBravoDelfina/multitud.png');
 }
 
 function setup() {
   createCanvas(1366, 768);
   marAbierto = loadImage('../mediaBravoDelfina/marabierto.png');
   marCerrado = loadImage('../mediaBravoDelfina/marcerrado.png');
-  moises = loadImage('../Media_ChavezFernanda/moisesvara.png');
-  multitud = loadImage('../mediaBravoDelfina/multitud.png');
+  
   pulgar = loadImage('../Media_ChavezFernanda/Pulgar_Arriba.gif');
 
   y = height; // Inicializa la posición y en la parte inferior del lienzo
   fondo = marAbierto; // Inicializa el fondo con marAbierto
 
   // Redimensiona la imagen de Moisés manteniendo la proporción
-  let newWidth = moises.width * 2.5; // Multiplica por 0.8 para hacer la imagen más pequeña
-  let newHeight = moises.height * 2.5; // Multiplica por 0.8 para hacer la imagen más pequeña
+  let newWidth = moises.width * 1.6; // Multiplica por 0.8 para hacer la imagen más pequeña
+  let newHeight = moises.height * 1.6; // Multiplica por 0.8 para hacer la imagen más pequeña
   moises.resize(newWidth, newHeight);
 
   // Redimensiona la imagen de la multitud manteniendo la proporción
-  let nuevoancho = multitud.width * 1.7; 
-  let nuevoalto = multitud.height * 1.7; // Multi
+  let nuevoancho = multitud.width * 1.5; 
+  let nuevoalto = multitud.height * 1.5; // Multi
   multitud.resize(nuevoancho, nuevoalto);
 }
 
 function draw() {
   background(fondo);
-  
+  audioD.play();
   if (y >= -moises.height) { // Dibuja la imagen solo si aún no ha salido de la pantalla
-    image(multitud, 123, y+300); // Dibuja la imagen redimensionada de la multitud
-    image(moises, 420, y - 120); // Dibuja la imagen redimensionada de Moisés un poco más arriba de la multitud
-    y -= 2; // Mueve la imagen hacia arriba (decrementa y)
+    image(multitud, 150, y + 300); // Dibuja la imagen redimensionada de la multitud
+    image(moises, 150, y - 150); // Dibuja la imagen redimensionada de Moisés un poco más arriba de la multitud
+    y -= 1.5; // Mueve la imagen hacia arriba (decrementa y)
     
   } else {
     fondo = marCerrado; // Cambia el fondo a marCerrado
